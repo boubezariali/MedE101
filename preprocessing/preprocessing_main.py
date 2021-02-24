d3ad07ea376532202a1ce2a1d45e7fcb1241a9db
Ali Boubezari <boubezari.ali@gmail.com>
2021-02-23 23:00:20 -0800

    finalized script and added usage

--- preprocessing/preprocessing_main.py
+++ preprocessing/preprocessing_main.py
@@ -1,4 +1,7 @@
 from preprocessing.sentence_splitting_utils import *
 
 def main(): 
-   print(split_into_sentences_regex('hello there. my name is ali')) 
+   print(split_into_sentences_regex('Hello world! Nice to meet you.')) 
+
+if __name__ == '__main__': 
+    main()
