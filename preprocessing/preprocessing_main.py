1178242b99d0a94599d8bfe0639c7fb80799c1a7
Ali Boubezari <boubezari.ali@gmail.com>
2021-02-25 18:21:24 -0800

    added rudementary utility files

--- preprocessing/preprocessing_main.py
+++ preprocessing/preprocessing_main.py
@@ -1,7 +1,33 @@
 from preprocessing.sentence_splitting_utils import *
+import argparse
 
-def main(): 
-   print(split_into_sentences_regex('Hello world! Nice to meet you.')) 
 
-if __name__ == '__main__': 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--split_method",
+        type=str,
+        help="Sentence splitting method to use. Can be regex or nltk",
+    )
+    parser.add_argument("--input_file", type=str, help="Path of file to process.")
+    parser.add_argument(
+        "--output_file", type=str, default="", help="Output location of processed text."
+    )
+    args = parser.parse_args()
+    assert args.input_file is not None, "Input file must be non-empty"
+
+    with open(args.input_file, "r") as f:
+        text = f.read()
+    if args.split_method == "regex":
+        split_data = split_into_sentences_regex(text)
+    elif args.split_method == "nltk":
+        split_data = split_into_sentences_nltk(text)
+    else:
+        raise ValueError("Invalid option {}.".format(args.split_method))
+    with open(args.output_file, "w") as f:
+        result = "\n".join(split_data)
+        f.write(result)
+
+
+if __name__ == "__main__":
     main()
--- preprocessing/sentence_splitting_utils.py
+++ preprocessing/sentence_splitting_utils.py
@@ -1,43 +1,60 @@
-# Utility file for splitting sentences. 
+""" Utility file for splitting sentences. 
+"""
 import re
 import nltk.data
 
-# Regex representations (utf-8 encoded). 
-alphabets= "([A-Za-z])"
-prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
-suffixes = "(Inc|Ltd|Jr|Sr|Co)"
-starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
-acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
-websites = "[.](com|net|org|io|gov)"
 
 def split_into_sentences_regex(text):
+    """Split into sentences using regex.
+    :param text: a string to split into sentences.
+    :return: list of sentences.
+    """
+    alphabets = "([A-Za-z])"
+    prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+    suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+    starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+    acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+    websites = "[.](com|net|org|io|gov)"
+
     text = " " + text + "  "
-    text = text.replace("\n"," ")
-    text = re.sub(prefixes,"\\1<prd>",text)
-    text = re.sub(websites,"<prd>\\1",text)
-    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
-    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
-    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
-    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
-    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
-    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
-    if "”" in text: text = text.replace(".”","”.")
-    if "\"" in text: text = text.replace(".\"","\".")
-    if "!" in text: text = text.replace("!\"","\"!")
-    if "?" in text: text = text.replace("?\"","\"?")
-    text = text.replace(".",".<stop>")
-    text = text.replace("?","?<stop>")
-    text = text.replace("!","!<stop>")
-    text = text.replace("<prd>",".")
+    text = text.replace("\n", " ")
+    text = re.sub(prefixes, "\\1<prd>", text)
+    text = re.sub(websites, "<prd>\\1", text)
+    if "Ph.D" in text:
+        text = text.replace("Ph.D.", "Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] ", " \\1<prd> ", text)
+    text = re.sub(acronyms + " " + starters, "\\1<stop> \\2", text)
+    text = re.sub(
+        alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]",
+        "\\1<prd>\\2<prd>\\3<prd>",
+        text,
+    )
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>", text)
+    text = re.sub(" " + suffixes + "[.] " + starters, " \\1<stop> \\2", text)
+    text = re.sub(" " + suffixes + "[.]", " \\1<prd>", text)
+    text = re.sub(" " + alphabets + "[.]", " \\1<prd>", text)
+    if "”" in text:
+        text = text.replace(".”", "”.")
+    if '"' in text:
+        text = text.replace('."', '".')
+    if "!" in text:
+        text = text.replace('!"', '"!')
+    if "?" in text:
+        text = text.replace('?"', '"?')
+    text = text.replace(".", ".<stop>")
+    text = text.replace("?", "?<stop>")
+    text = text.replace("!", "!<stop>")
+    text = text.replace("<prd>", ".")
     sentences = text.split("<stop>")
     sentences = sentences[:-1]
     sentences = [s.strip() for s in sentences]
     return sentences
 
-def split_into_sentences_nltk(filename): 
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    fp = open(filename)
-    data = fp.read()
-    return '\n-----\n'.join(tokenizer.tokenize(data))
+
+def split_into_sentences_nltk(text):
+    """Split into sentences using nltk tokenizer..
+    :param text: a string to split into sentences.
+    :return: list of sentences.
+    """
+    tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
+    return "\n-----\n".join(tokenizer.tokenize(text))
