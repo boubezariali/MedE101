eaa988c0a9248bf7d8f44d386fd425ae160d4e0a
Ali Boubezari <boubezari.ali@gmail.com>
2021-03-01 12:13:37 -0800

    tried reformatting comments

--- file_utils/delimeted_file.py
+++ file_utils/delimeted_file.py
@@ -6,16 +6,19 @@ DELIMETER = "@@"
 
 
 class DelimetedFile:
-    """Class that represents a delimeted file. This class contains utilities
-    for reading, writing, and handling the delimeted file.
+    """Class that represents a delimeted file.
+    This class contains utilities for reading, writing, and handling the
+    delimeted file.
     """
 
     def __init__(self, path, overwrite=False):
         """Initialize the DelimetedFile.
-        :param path: file path to read/write from.
-        :param overwrite: flag to overwrite pre-existing data in the filepath or
-            not. Setting this to true will reset the file and any data will
-            be lost.
+
+        Args:
+            param path: file path to read/write from.
+            param overwrite: flag to overwrite pre-existing data in the filepath or
+                not. Setting this to true will reset the file and any data will
+                be lost.
         """
         assert os.path.isfile, "Input a valid file path."
         self.path = path
@@ -27,18 +30,24 @@ class DelimetedFile:
                 self._lst = txt.split(DELIMETER)
 
     def write(self):
-        """Write any data stored in the object to disk."""
+        """ Write any data stored in the object to disk."""
         with open(self.path, "w") as f:
             txt = DELIMETER.join(self._lst)
             f.write(txt)
 
     def add(self, lst):
-        """Add a list of strings to the existing data stored int he object.
-        :param lst: list of strings to add to the object.
+        """Add a list of strings to the existing data stored in the object.
+
+        Args:
+            param lst: list of strings to add to the object.
         """
         self._lst.extend(lst)
 
     @property
     def lst(self):
-        """Return a copy of the stored data in the object."""
+        """Return a copy of the stored data in the object.i
+
+        Returns:
+            Copy of internal list of strings.
+        """
         return np.copy(self._lst)
