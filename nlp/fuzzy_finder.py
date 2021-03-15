19f9b359752d79cbff7b1e20ed962ad9cef7b5e3
Ali Boubezari <boubezari.ali@gmail.com>
2021-03-14 23:40:00 -0700

    saving progress

--- nlp/fuzzy_finder.py
+++ nlp/fuzzy_finder.py
@@ -1,5 +1,6 @@
 from file_utils.array_io_utils import read_array
-from preprocessing.string_cleaning_utils import get_stopwords, get_punctuation, remove_stopstrings, remove_stopchars
+from preprocessing.string_cleaning_utils import get_stopwords, get_punctuation, remove_stopstrings, remove_stopchars, stem
+from fuzzyset import FuzzySet
 
 import glog
 import pandas as pd
@@ -17,7 +18,32 @@ class FuzzyFinder:
         for elem in array:
             words = elem[0].split(" ")  
             words = remove_stopstrings(words, stopwords)
-            words = remove_stopchars(words, punctuation)
+            words = [remove_stopchars(word, punctuation) for word in words]
+            words = [stem(word) for word in words]
             self.contents.append(set(words))
         glog.info("Load data complete.")
+
+    def get_features_from_sentence(self, sentence):
+        sentence = [stem(word) for word in sentence]
+        print(sentence)
+        fuzzy_set = FuzzySet(sentence)
+        result = []
+        for elem in self.contents:
+            valid_feature = True
+            for word in elem:
+                score = fuzzy_set.get(word)
+                if score is None or score[0][0] < 0.75:
+                    valid_feature = False
+                    break
+            if valid_feature:
+                result.append(elem)
+        return result
+                
+                
+
+                
+        
+
+
+        
 
--- preprocessing/string_cleaning_utils.py
+++ preprocessing/string_cleaning_utils.py
@@ -2,12 +2,24 @@
 """
 import nltk
 import string
+from nltk.stem.snowball import SnowballStemmer
 
 from file_utils.array_io_utils import lined_file_to_array
 
 STOPWORDS_FILE = "preprocessing/stopwords.txt"
 PUNCTUATION_FILE = "preprocessing/punctuation.txt"
 
+
+def get_stopwords():
+    """Get the global list of english stopwords from the predetermined file."""
+    array = lined_file_to_array(STOPWORDS_FILE)
+    return set(array)
+
+
+def get_punctuation():
+    """Get the global list of punctuation from the predetermined file."""
+    array = lined_file_to_array(PUNCTUATION_FILE)
+    return set(array)
 
 def word_tokenize(string):
     """Tokenize a string into individual words.
@@ -29,25 +41,25 @@ def remove_stopstrings(strings, stopstrings):
     """
     return [s for s in strings if s not in stopstrings]
 
-def remove_stopchars(strings, stopchars):
+def remove_stopchars(string, stopchars):
     """Removes a list of characters from the words, modifying the words
     themselves. 
     Args:
-        param strings: list of input strings to clean.
+        param strings: a string to clean.
         param stopchars: list of strings to check for in the words.
     Returns:
-        list of strings
+        the cleaned string
     """
     stopchars_str = ''.join(stopchars)
-    return [s.translate(str.maketrans('', '', stopchars_str)) for s in strings]
+    return string.translate(str.maketrans('', '', stopchars_str)) 
 
-def get_stopwords():
-    """Get the global list of english stopwords from the predetermined file."""
-    array = lined_file_to_array(STOPWORDS_FILE)
-    return set(array)
-
-
-def get_punctuation():
-    """Get the global list of punctuation from the predetermined file."""
-    array = lined_file_to_array(PUNCTUATION_FILE)
-    return set(array)
+def stem(string):
+    """Gets the stem or base of an english word. If the word is not
+    english or invalid, the input string will be returned.
+    Args:
+        string: string to process.
+    Returns:
+        the stem of the string.
+    """
+    stemmer = SnowballStemmer("english")
+    return stemmer.stem(string)
