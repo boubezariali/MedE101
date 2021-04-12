8c7293ba31e1fc31a91f91e01035b1e39790618f
laurku <48036026+laurku@users.noreply.github.com>
2021-04-12 10:30:08 -0700

    [App] Feature autocomplete on UI (#43)

--- app/static/app_front.js
+++ app/static/app_front.js
@@ -1,15 +1,52 @@
 /*
- * Scripting for html pages goes here.
+ * Scripting for html pages goes here. Using JQuery.
  */
-(function() {
+$(function() {
     "use strict";
     console.log("in javascript");
+
+    // Clinical  features - replace with load CSV
+    let availableTags = ["pain", "paralysis", "cough", "fever"];
+
+    // Configure autocomplete for #tags ui-widget
+    $('#tags')
+    .on("keydown", function(event) {
+        if (event.keyCode === $.ui.keyCode.TAB && 
+            $(this).autocomplete("instance").menu.active) {
+            event.preventDefault();
+        }
+    })
+    .autocomplete({
+        minLength: 0,
+        source: function(request, response) {
+            response($.ui.autocomplete.filter(availableTags, extractLast(request.term)));
+        },
+        focus: function() {
+            return false;
+        },
+        select: function(event, ui) {
+            let terms = split(this.value);
+            terms.pop();
+            terms.push(ui.item.value);
+            terms.push("");
+            this.value = terms.join(", ");
+            return false;
+        }
+    });
     
-    
-    // fetch json
+    // Fetch json
       fetch('/test')
           .then(function (text) {
               let textBlock = 'GET response text: \n' + text + '\n' + JSON.stringify(text) + '\n';
               document.querySelector("#mylog").textContent = textBlock;
           })
-})();
+
+    // Simple helper functions
+    function split(val) {
+        return val.split(/, \s*/);
+    }
+
+    function extractLast(term) {
+        return split(term).pop();
+    }
+});
