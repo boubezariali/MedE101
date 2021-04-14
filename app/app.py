7d1360b0c67ae4be531a7d549772b1814083c831
Laura Hu <lauradhu@hotmail.com>
2021-04-14 10:27:30 -0700

    [App] Succesffuly pass input/output between frontend and Python backend

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect
 app = Flask(__name__)
 
 
@@ -9,19 +9,38 @@ def home_page():
     example_embed='This sample string is from Python, passed into HTML frontend via Flask!'
     return render_template('index.html', embed=example_embed)
 
-######## Example fetch ############
-@app.route('/test', methods=['GET', 'POST'])
-def testfn():
-    message = "default"
-
-    # POST request
+@app.route('/submitfn', methods = ['POST', 'GET'])
+def submitfn():
     if request.method == 'POST':
-        print(request.get_json())
-        message = ('OK', 200)
-    # GET request
+        symp = request.form['symptom']
+        print("symp is ", symp)
+        msg = testHelper(symp)
+        return render_template('index.html', disease_output=msg)
     else:
-        message = {'greeting':'Hello from Flask!'}
-    return render_template('test_page.html', mylog=message)
+        return redirect('/')
+
+###### Backend Helpers ###########
+# Take frontend input and convert to training data point
+# input = format_input(frontend_input)
+
+# Elsewhere train and save model e.g. pickle.dump(model, open(filename, 'wb'))
+# Load trained model e.g. pickle.load(open(filename, 'rb'))
+
+# Generate model prediction
+# output = model.predict(input)
+
+# Pass prediction to front page
+# render_template('index.html', output_display=output)
+
+
+# Dummy to test processing input data
+def testHelper(input):
+    diseases = ["heart attack", "flu", "cancer", "allergy"]
+    pick = len(input) % len(diseases)
+    msg = "Disease: " + diseases[pick]
+    print(msg)
+    return msg
+
 
 
 # run app
