21787ebf87d92c9e0b9997c60b60ff01377bae82
boubezariali <boubezari.ali@gmail.com>
2021-04-19 19:48:54 -0700

    updated negation feature extractor (#48)

--- nlp/feature_extractors/negation_feature_extractor.py
+++ nlp/feature_extractors/negation_feature_extractor.py
@@ -3,63 +3,41 @@ Class definition for NegationFeatureExtractor.
 """
 from file_utils.array_io_utils import lined_file_to_array
 from nlp.feature_extractors.feature_extractor import FeatureExtractor
+from nlp.stanza_utils import get_stanza_model
 import re
+from preprocessing.string_cleaning_utils import (get_punctuation,
+                                                 get_stopwords,
+                                                 remove_stopchars,
+                                                 remove_stopstrings, stem)
 
-CLAUSE_PUNCT = ['but', '.', ';']  # Identifiy clausal breaks
-NEG_WORDS = list(lined_file_to_array('nlp/negation_words.txt'))  # Negation words
-FEATURE_LENGTH = 4
+CLAUSE_PUNCT = ['but', '\.', ';']  # Identifiy clausal breaks
+NEG_WORDS = set(lined_file_to_array('nlp/negation_words.txt'))  # Negation words
 
 class NegationFeatureExtractor(FeatureExtractor): 
     """
-    Extracts negated features, for now as list.
+    Extracts all negated words, for now as list.
     """
 
     def get_features(self, text):
         """ text - string of text document """
-        # Split text into list of words and punctuation
-        split_text = re.findall(r"[\w']+|[.,!?;]", text)
-        negated_words = [[]] # different sublist per clause
-        negated_features = []
-        results = []
-        scope_is_neg = False
-        idx = 0
-        for i, word in enumerate(split_text):
-            # Update negation scope
-            if word in NEG_WORDS:
-                scope_is_neg = not scope_is_neg
-            
-            # If new clause, reset to no negation and start new sublist
-            elif word in CLAUSE_PUNCT:
-                scope_is_neg = False
-                negated_words.append([])
-                idx += 1
-            
-            # While negation scope is true, words are negated
-            else:
-                if scope_is_neg:
-                    negated_words[idx].append(word) 
-
-        # Now we have negated words, identify negated features (multiple words)
-        for feature in self._keyword_handler.keywords:
-            # Look for consecutive words representing a feature
-            for clause_list in negated_words:
-                for wsize in range(0, FEATURE_LENGTH+1):
-                    left = 0
-                    right = wsize
-                    while right < len(clause_list) + 1:
-                        feature_count = 0
-                        # Every word in window must be part of set of words for the feature
-                        for i in range(left, right):
-                            if clause_list[i] in feature:
-                                feature_count += 1
-                        if feature_count == len(feature):
-                            negated_features.append(feature)
-                        left += 1
-                        right += 1
-        # clean up
+        nlp = get_stanza_model(processors='tokenize', download=False)
+        doc = nlp(text) 
         result = []
-        for item in negated_features:
-            if item not in result and len(item)>0:
-                result.append(item)
-        return result
-
+        # Get all sentences in the input text.
+        sentences = [sentence.text for sentence in doc.sentences]
+        for sentence in sentences: 
+            # Split the sentence into its clauses seperated by the clause punctutation.
+            clauses = re.split('|'.join(CLAUSE_PUNCT), sentence)
+            for clause in clauses:  
+                # Clean up each clause.
+                clause = clause.split(' ')
+                clause = remove_stopstrings(clause, self._keyword_handler.stopwords)
+                clause = [remove_stopchars(word, self._keyword_handler.punctuation) for word in clause]
+                clause = [stem(word) for word in clause]
+                is_neg = False
+                for word in clause: 
+                    if is_neg: 
+                        result.append(word)
+                    if word in NEG_WORDS:
+                       is_neg = True
+        return result 
