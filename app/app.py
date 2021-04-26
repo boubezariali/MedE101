d4a289a34f8d9ce8b802470d90ac6d28ef8d47bb
boubezariali <boubezari.ali@gmail.com>
2021-04-26 14:52:15 -0700

    reformat (#57)

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 # app.py
+from flask import Flask, jsonify, redirect, render_template, request
+
 from helpers import testHelper
-from flask import Flask, jsonify, request, render_template, redirect
+
 app = Flask(__name__)
 
 # TODO: replace global with database
@@ -9,17 +11,21 @@ INPUT_FEATURES = []
 ######## HOME ############
 @app.route('/')
 def home_page():
-    example_embed='This sample string is from Python, passed into HTML frontend via Flask!'
+    example_embed = (
+        'This sample string is from Python, passed into HTML frontend via Flask!'
+    )
     return render_template('index.html', embed=example_embed)
 
+
 # Process GET requests for diagnosis
-@app.route('/retrieve_diagnosis', methods = ['GET'])
+@app.route('/retrieve_diagnosis', methods=['GET'])
 def retrieve_diagnosis():
     diagnosis = testHelper(INPUT_FEATURES)
     return jsonify({'diagnosis': diagnosis})
 
+
 # Process POST request with input features from user
-@app.route('/add_feature', methods = ['POST'])  
+@app.route('/add_feature', methods=['POST'])
 def add_feature_old():
     global INPUT_FEATURES
     feature_data = request.get_json()
@@ -27,15 +33,16 @@ def add_feature_old():
     return 'Done', 201
 
 
-@app.route('/submitfn', methods = ['POST', 'GET'])
+@app.route('/submitfn', methods=['POST', 'GET'])
 def submitfn():
     if request.method == 'POST':
-        symp = request.form['tags'] # comma separated string
+        symp = request.form['tags']  # comma separated string
         print("symp is ", symp, len(symp))
         msg = testHelper(symp)
         return render_template('index.html', disease_output=msg)
     else:
         return redirect('/')
+
 
 ###### Backend Helpers ###########
 # Take frontend input and convert to training data point
