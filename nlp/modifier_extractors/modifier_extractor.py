5659d33415740a7c089aa8d944e249a59f8f4750
boubezariali <boubezari.ali@gmail.com>
2021-04-26 21:06:28 -0700

    added documentation (#59)

--- nlp/modifier_extractors/modifier_extractor.py
+++ nlp/modifier_extractors/modifier_extractor.py
@@ -5,13 +5,26 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 class ModifierExtractor(ABC):
+    """ This class is responsible for finding modifiers for each word 
+    in a given text.
+    """
 
     def __init__(self, keyword_handler, modifier_handler):
+        """ The class requires a KeywordHandler for string cleaning and
+        a ModifierHandler to store the extracted modifiers.
+
+        Args:
+            keyword_handler: KeywordHandler
+            modifier_handler: ModifierHandler
+        """
         self._keyword_handler = keyword_handler
         self._modifier_handler = modifier_handler        
     
     @abstractmethod
     def get_modifiers(self, text):
+        """ This function will extract all modifiers and store them in
+        the ModifierHandler attribute.
+        """
         pass
         
 
--- nlp/modifier_handler.py
+++ nlp/modifier_handler.py
@@ -1,20 +1,44 @@
+""" Class definition for base ModifierExtractor.
+"""
 from collections import defaultdict
 
 from nlp.stanza_utils import get_stanza_model
 
 
 class ModifierHandler:
+    """ This class stores modifiers in a indexed map. 
+    """
+
     def __init__(self):
+        # Initialize the map such that each word represents a list.
         self._modifiers = defaultdict(list)
 
     def add_modifier(self, sent_idx, word_idx, modifier):
+        """ Add a modifier to a word.
+        
+        Args: 
+            sent_idx: int representing which sentence the word is in.
+            word_idx: int identifying the word within the sentence.
+            modifier: string modifier.
+        """
         self._modifiers[(sent_idx, word_idx)].append(modifier)
 
     def get_modifier(self, sent_idx, word_idx):
+        """ Access the modifiers of a word.
+
+        Args:
+            sent_idx: int representing which sentence the word is in.
+            word_idx: int identifying the word within the sentence.
+
+        Returns:
+            list of modifiers.
+        """
         if (sent_idx, word_idx) in self._modifiers:
             return self._modifiers[sent_idx, word_idx]
         return []
 
     @property
     def modifiers(self):
+        """ Getter function for the modifier map.
+        """
         return self._modifiers
